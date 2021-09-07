f7a53c3c9d4eb399e21538b848730bc93dbd711f
Abhinav <in.abhi9@gmail.com>
2021-09-07 20:08:22 +0530

    chore: init config

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# Keypad config
+
+keypad_cols = (0, 1, 2, 3)
+keypad_rows = (4, 5, 7, 6)
+
+keypad_keymap = [('1', '2', '3', 'A'),
+                 ('4', '5', '6', 'B'),
+                 ('7', '8', '9', 'C'),
+                 ('*', '0', '#', 'D')]
+
+# HID Macro config
+
+hid_macro_short_modifier = 'CTRL'
+hid_macro_long_modifier = 'ALT-CTRL'
