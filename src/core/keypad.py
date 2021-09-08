fe326aa4680521f800fce9ac2f2c85f35b29bcfa
Abhinav <in.abhi9@gmail.com>
2021-09-08 20:05:02 +0530

    fix(keypad): missed key and freeze

--- src/core/keypad.py
+++ src/core/keypad.py
@@ -18,10 +18,13 @@ class Keypad(Matrix_Keypad):
     def pressed_key(self):
         pressed_key = (self.pressed_keys or [None])[0]
 
+        if pressed_key:
+            l.debug('pressed key:', pressed_key, self._tick_down, self._tick_up)
+
         if not pressed_key and self._tick_down and not self._tick_up:
             self._tick_up = time.ticks_ms()
 
-        if pressed_key and not self._tick_down:
+        if pressed_key and not self._tick_down and not self._tick_up:
             self._pk = pressed_key
             self._tick_down = time.ticks_ms()
 
@@ -33,19 +36,17 @@ class Keypad(Matrix_Keypad):
             if diff == self._lng_delay:
                 l.debug('Key longpress:', self._pk)
                 self._sent = True
-                time.sleep_ms(10)  # otherwise freezes for some reason
                 return self._pk, True
 
         if self._tick_up:
             diff = time.ticks_diff(time.ticks_ms(), self._tick_up)
-            if diff == 10:
+            if diff > 10:
                 l.debug('Key up:', self._pk)
 
                 self._tick_up = None
                 self._tick_down = None
 
                 if not self._sent:
-                    time.sleep_ms(10)  # otherwise freezes for some reason
                     return self._pk, False
 
         return None, False
