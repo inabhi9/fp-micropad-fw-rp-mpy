7cc138e529fde8c42e9a7c9c82f22c3bbbc589bb
Abhinav <in.abhi9@gmail.com>
2021-09-28 18:52:58 +0530

    refactor(feedback): use event driven architecture to provide feedback

--- src/feedback.py
+++ src/feedback.py
@@ -1,107 +1,119 @@
 import uasyncio
-from machine import Pin, PWM
 
-from core import state
+import config
+from core import events, feedback_utils
+from core import state, l
+from core.feedback_utils import vibrator_on, vibrator_off
 
 
 class Feedback:
     _running = False
-    LED_PIN_BLUE = 13
-    LED_PIN_RED = 14
-
-    def __init__(self, visual=True, haptic=False):
-        self._visual = visual
-        self._haptic = haptic
 
     async def task(self):
         self._running = True
+
+        boot_auth_prev_state = None
+        boot_auth_invalid_prev_state = state.BOOT_AUTH_INVALID
+        fp_auth_prev_state = state.FP_VERIFIED
+        fp_auth_invalid_prev_state = state.FP_VERIFIED_INVALID
+
         while self._running:
-            self._schedule_task(state.BOOT_AUTH, False, self._task_boot_auth)
-            self._clear_task(state.BOOT_AUTH, True, self._task_boot_auth)
+            awaitables = []
 
-            self._schedule_task(state.BOOT_AUTH_INVALID, True, self._task_boot_auth_invalid)
-            self._clear_task(state.BOOT_AUTH_INVALID, False, self._task_boot_auth_invalid)
+            if boot_auth_prev_state != state.BOOT_AUTH:
+                awaitables.append(events.post_boot_auth_change.send(boot_auth=state.BOOT_AUTH))
 
-            self._schedule_task(state.FP_VERIFIED, True, self._task_fp_verified)
-            self._clear_task(state.FP_VERIFIED, False, self._task_fp_verified)
+            if fp_auth_prev_state != state.FP_VERIFIED and state.FP_VERIFIED:
+                awaitables.append(events.fp_auth.send())
 
-            self._schedule_task(state.FP_VERIFIED_INVALID, True, self._task_fp_verified_invalid)
-            self._clear_task(state.FP_VERIFIED_INVALID, False, self._task_fp_verified_invalid)
+            if (fp_auth_invalid_prev_state != state.FP_VERIFIED_INVALID
+                    and state.FP_VERIFIED_INVALID):
+                awaitables.append(events.fp_auth_invalid.send())
 
+            if boot_auth_invalid_prev_state != state.BOOT_AUTH_INVALID and state.BOOT_AUTH_INVALID:
+                awaitables.append(events.boot_auth_invalid.send())
+
+            boot_auth_prev_state = state.BOOT_AUTH
+            boot_auth_invalid_prev_state = state.BOOT_AUTH_INVALID
+            fp_auth_prev_state = state.FP_VERIFIED
+            fp_auth_invalid_prev_state = state.FP_VERIFIED_INVALID
+
+            [uasyncio.create_task(t) for t in awaitables]
             await uasyncio.sleep_ms(10)
 
-    async def _task_boot_auth(self):
-        # Construct PWM object, with LED on Pin(25).
-        pwm = PWM(Pin(self.LED_PIN_BLUE))
 
-        # Set the PWM frequency.
-        pwm.freq(1000)
+@events.on(events.post_boot_auth_change)
+async def haptic_on_boot_auth(**kwargs):
+    if kwargs.get('boot_auth') is True:
+        l.debug('haptic_on_boot_auth')
+        vibrator_on()
+        await uasyncio.sleep_ms(200)
+        vibrator_off()
 
-        while state.BOOT_AUTH is False:
-            # Fade the LED in and out a few times.
-            duty = 0
-            direction = 1
-            for _ in range(2 * 256):
 
-                if state.BOOT_AUTH:
-                    Pin(self.LED_PIN_BLUE, mode=Pin.OUT).low()
-                    break
+@events.on(events.fp_auth)
+async def haptic_on_fp_auth(**kwargs):
+    l.debug('haptic_on_fp_auth')
+    vibrator_off()
+    await uasyncio.sleep_ms(10)
+    vibrator_on()
+    await uasyncio.sleep_ms(150)
+    vibrator_off()
 
-                duty += direction
-                if duty > 255:
-                    duty = 255
-                    direction = -1
-                elif duty < 0:
-                    duty = 0
-                    direction = 1
-                pwm.duty_u16(duty * duty)
-                await uasyncio.sleep_ms(5)
 
-    async def _task_boot_auth_invalid(self):
-        pin = Pin(self.LED_PIN_RED, mode=Pin.OUT)
-        pin.high()
-        await uasyncio.sleep(2)
-        pin.low()
+@events.on([events.fp_auth_invalid, events.boot_auth_invalid])
+async def haptic_on_fp_auth_invalid(**kwargs):
+    for i in range(2):
+        l.debug('haptic_on_fp_auth_invalid')
+        vibrator_on()
+        await uasyncio.sleep_ms(100)
+        vibrator_off()
+        await uasyncio.sleep_ms(200)
 
-    async def _task_fp_verified_invalid(self):
-        await self._task_boot_auth_invalid()
 
-    async def _task_fp_verified(self):
-        # Construct PWM object, with LED on Pin(25).
-        pwm = PWM(Pin(self.LED_PIN_BLUE))
+@events.on(events.post_boot_auth_change)
+async def visual_on_boot_auth(**kwargs):
+    if kwargs.get('boot_auth') is False:
+        return
 
-        # Set the PWM frequency.
-        pwm.freq(50)
+    feedback_utils.led_on(feedback_utils.LED_PIN_GREEN)
+    await uasyncio.sleep_ms(500)
+    feedback_utils.led_off(feedback_utils.LED_PIN_GREEN)
 
-        while state.FP_VERIFIED:
-            # Fade the LED in and out a few times.
-            duty = 0
-            direction = 1
-            for _ in range(2 * 256):
-                duty += direction
-                if duty > 255:
-                    duty = 255
-                    direction = -1
-                elif duty < 0:
-                    duty = 0
-                    direction = 1
-                pwm.duty_u16(duty * duty)
-                await uasyncio.sleep_ms(1)
 
-    def _clear_all(self):
-        Pin(self.LED_PIN_BLUE, mode=Pin.OUT).low()
-        Pin(self.LED_PIN_RED, mode=Pin.OUT).low()
+@events.on(events.post_boot_auth_change)
+async def visual_on_boot_not_auth(**kwargs):
+    if kwargs.get('boot_auth') is True:
+        return
 
-    def _schedule_task(self, state_val, eq_val, task):
-        task_inst_name = 'taskinst_%s' % task.__name__
-        task_inst = getattr(self, task_inst_name, None)
-        if state_val == eq_val and not task_inst:
-            task_inst = uasyncio.create_task(task())
-            setattr(self, task_inst_name, task_inst)
+    while state.BOOT_AUTH is False:
+        await feedback_utils.pwm(feedback_utils.LED_PIN_BLUE, peak=100, delay_cycle=5,
+                                 break_on=lambda: state.BOOT_AUTH)
 
-    def _clear_task(self, state_val, eq_val, task):
-        task_inst_name = 'taskinst_%s' % task.__name__
-        task_inst = getattr(self, task_inst_name, None)
-        if state_val == eq_val and task_inst:
-            del task_inst
-            setattr(self, task_inst_name, None)
+
+@events.on(events.fp_auth)
+async def visual_on_fp_auth(**kwargs):
+    while state.FP_VERIFIED:
+        await feedback_utils.pwm(feedback_utils.LED_PIN_GREEN, peak=100, delay_cycle=2,
+                                 break_on=lambda: not state.FP_VERIFIED)
+
+
+@events.on(events.fp_auth_invalid)
+async def visual_on_fp_auth_invalid(**kwargs):
+    feedback_utils.led_on(feedback_utils.LED_PIN_RED)
+    await uasyncio.sleep(1)
+    feedback_utils.led_off(feedback_utils.LED_PIN_RED)
+
+
+@events.on(events.keypress)
+async def visual_on_keypress(**kwargs):
+    feedback_utils.led_on(feedback_utils.LED_PIN_BLUE)
+    await uasyncio.sleep_ms(10)
+    feedback_utils.led_off(feedback_utils.LED_PIN_BLUE)
+
+
+@events.on(events.boot_auth_invalid)
+async def visual_on_boot_auth_invalid(**kwargs):
+    feedback_utils.led_on(feedback_utils.LED_PIN_RED)
+    await uasyncio.sleep(config.DELAY_AFTER_INVALID_BOOT_AUTH)
+    feedback_utils.led_off(feedback_utils.LED_PIN_RED)
